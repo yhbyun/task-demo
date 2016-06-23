a7ef555d763f103c6948a78a7a6244dd861cb94c
YongHun Byun <yonghunbyun@gmail.com>
2016-06-23 15:26:22 +0900

    Add tasks route

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -14,3 +14,13 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/tasks', ['as' => 'tasks.index', 'uses' => 'TasksController@index']);
+Route::get('/tasks/create', ['as' => 'tasks.create', 'uses' => 'TasksController@create']);
+Route::get('/tasks/{id}', ['as' => 'tasks.show', 'uses' => 'TasksController@show']);
+Route::post('/tasks', ['as' => 'tasks.store', 'uses' => 'TasksController@store']);
+Route::get('/tasks/{id}/edit', ['as' => 'tasks.edit', 'uses' => 'TasksController@edit']);
+Route::put('/tasks/{id}', ['as' => 'tasks.update', 'uses' => 'TasksController@update']);
+Route::delete('/tasks/{id}', ['as' => 'tasks.delete', 'uses' => 'TasksController@destroy']);
+
